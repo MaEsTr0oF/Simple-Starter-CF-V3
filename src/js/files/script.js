9dfdc359d76688882117df6917302820676adb98
MaEsTr0oF <114798814+MaEsTr0oF@users.noreply.github.com>
2024-08-15 21:34:21 +0300

    Version 1.0 beta only laptop

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,3 +3,56 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+document.addEventListener('scroll', () => {
+   const hiddenText = document.querySelectorAll('.off');
+   hiddenText.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+         element.classList.add('on');
+     }
+   });
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slide');
+  var dots = document.getElementsByClassName('dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+
+  }
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+var i = 1;
+  setInterval(function(){
+  currentSlide(i)
+  i++;
+  if(i == 5)
+  {
+   i = 1;
+  }
+},4000)
+
+
