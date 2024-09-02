6351ca3527722f24764f8f4b737fb99fb9c94470
Adel Alzoabi <adel.krut.qwerty@gmail.com>
2024-09-02 11:51:26 +0300

    added: cors

--- api/internal/handlers/user.go
+++ api/internal/handlers/user.go
@@ -37,7 +37,7 @@ func HandleCors(next http.Handler) http.Handler {
         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-        if r.Method == "OPTIONS" {
+        if r.Method == "POST" {
             return
         }
 
