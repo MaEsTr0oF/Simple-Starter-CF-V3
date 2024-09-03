c8592188fc32c02fddeb5c92aeecf80944c494bf
AlzoabiAF <AlzoabiAF@users.noreply.github.com>
2024-09-03 10:04:44 +0000

    deploy: 835317ea04e643034d71d250c9a1793d81be5a3f

--- js/app.js
+++ js/app.js
@@ -293,7 +293,8 @@
         const formData = new FormData(this);
         const nameSurename = formData.get("surename");
         const presenceChoice = formData.get("choosen");
-        fetch("http://localhost:8080/api", {
+        const apiUrl = process.env.NGROK_URL;
+        fetch(`${apiUrl}/api`, {
             method: "post",
             headers: {
                 "Content-Type": "application/json"
